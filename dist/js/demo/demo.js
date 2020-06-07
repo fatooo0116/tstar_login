d99031deffceb77aa9b3d2ad73779e27370d05ef
fatooo0116@gmail.com <cbsi940424>
2020-06-08 01:30:10 +0800

    final

--- dist/js/demo/demo.js
+++ dist/js/demo/demo.js
@@ -13,6 +13,26 @@ function demoCalc(){
 (function($){
     $(document).ready(function(){
 
+        $(".sendCode").on("click",function(){
+            let me = this;
+            var time = 60000;//倒數7秒
+            !function MyCounter(){
+              if(time<=0){
+                //倒數完成
+                $(me).find(".timer").text('');
+              }else{
+                console.log((time/1000) + " sec...");
+                setTimeout(MyCounter, 1000);
+                $(me).find(".timer").text(time/1000);
+              }
+              time-=1000; 
+              
+            }();
+
+        });
+
+
+
         $(".passwdbox").each(function(){
             let passout = $(this);
             let passbox = $(this).find('input[type="password"]');
@@ -25,6 +45,7 @@ function demoCalc(){
             });
             passbox.keyup(function(){
                 if(passbox.val()!=''){
+                    $(".sec_login.active").removeClass("sh_err");
                     passout.addClass('focus');
                     prevbox.find('.text').text(passbox.val());
                 }else{
@@ -35,39 +56,92 @@ function demoCalc(){
 
 
         
-        $('input[type="password"]').focusout(function(){
-        //    $(this).parent().removeClass('focus');
+        $(".sec_login .back").on("click",function(e){
+            e.preventDefault();
+
+            let cur_pos = $(this).attr('cur'); 
+            let prev_pos = $(this).attr('target'); 
+
+
+
+                           
+            $(".sec_login").eq(cur_pos).removeClass('sh_err');
+            $(".sec_login").eq(cur_pos).addClass('right');
+
+            setTimeout(function(){
+                $(".sec_login").eq(cur_pos).removeClass("active");    
+
+                $(".sec_login").eq(cur_pos).addClass("right");                      
+                $(".sec_login").eq(prev_pos).addClass("active");
+                $(".sec_login").eq(prev_pos).removeClass("left");
+            },300);
+                    
         });
 
 
 
-        var i = 0;
-        $(".next").on("click",function(){    
+       
+        $(".next").on("click",function(e){
+            e.preventDefault();    
 
-            let target = $(".sec_login").eq(i);
-
-             target.find('input').keypress(function(){
-                target.removeClass('sh_err');
-              });
-
-              
+            let btn = $(this);
+            let cur_pos = $(this).attr('cur'); 
+            let next_pos = $(this).attr('target'); 
 
 
-            if($(".sec_login").eq(i).find("input").val().trim()!=''){
-                if(($(".sec_login").length-1) == i){
-                    
+            if(btn.hasClass('end')){
+                if($(".sec_login").eq(cur_pos).find("input").val().trim()!=''){
                     $("#ex1").modal({
                         fadeDuration: 100
                       });
-                    return false;
+                }else{
+                    $(".sec_login").eq(cur_pos).addClass('sh_err');
                 }
+                
+                return false;
+            }
 
-                $(".sec_login").eq(i).removeClass('sh_err');
-                $(".sec_login").eq(i).removeClass("active");
-                $(".sec_login").eq(i+1).addClass("active");
-                i++;             
+
+
+
+            if(next_pos > cur_pos){
+                 /* next */                
+
+                let target = $(".sec_login").eq(cur_pos);
+
+                target.find('input').keypress(function(){
+                   target.removeClass('sh_err');
+                 });
+   
+   
+               if(btn.hasClass('skip') || $(".sec_login").eq(cur_pos).find("input").val().trim()!=''){
+
+   
+                   $(".sec_login").eq(cur_pos).removeClass('sh_err');
+                   $(".sec_login").eq(cur_pos).removeClass("active");
+                   $(".sec_login").eq(cur_pos).removeClass("right");
+                   $(".sec_login").eq(cur_pos).addClass("left");
+                   $(".sec_login").eq(next_pos).addClass("active");
+                   $(".sec_login").eq(next_pos).removeClass("right");
+                               
+               }else{
+                   $(".sec_login").eq(cur_pos).addClass('sh_err');
+               }
+
             }else{
-                $(".sec_login").eq(i).addClass('sh_err');
+                /* back */
+      
+
+                $(".sec_login").eq(cur_pos).removeClass('sh_err');
+                $(".sec_login").eq(cur_pos).addClass('right');
+    
+                setTimeout(function(){
+                    $(".sec_login").eq(cur_pos).removeClass("active");    
+    
+                    $(".sec_login").eq(cur_pos).addClass("right");                      
+                    $(".sec_login").eq(next_pos).addClass("active");
+                    $(".sec_login").eq(next_pos).removeClass("left");
+                },300);
             }
                         
         });
